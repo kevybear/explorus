aaf2589e3fe4cbf756f31db6b4557e8b5133791f
Maiki Rainton <maikir@berkeley.edu>
2016-11-13 03:37:50 -0800

    styled about search page and add some animation files

--- key.py
+++ key.py
@@ -1,2 +1,2 @@
-amadeus_key = _
-instagram_key = _
+amadeus_key = "mtiAVGMmhvJd0Feh385SLhcstxpNcnGm"
+instagram_key = "387829024.a9a03de.832693a120bc4da183e39c982f030a8c"
---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+var ready;
+(function ($) {
+	new WOW().init();
+	
+})(jQuery);
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
