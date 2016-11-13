61969e63124045266e6181ca3a45b5bcb93edf31
alvenw <alvenw@berkeley.edu>
2016-11-13 00:48:04 -0800

    cleaned up unnecessary code

--- hello.py
+++ hello.py
@@ -23,27 +23,5 @@ def hello(name=None):
 		return render_template('about.html', listPics=listPics, name=myCity)
 	return render_template('about.html', name=name)
 
-
-	myCity = None
- 	if request.method == 'POST':
-		myCity = request.form['search']
-  		print myCity
-  	
-		listPics = getPhotos(myCity)
-		c = []
-		for x in listPics:
-			if x[1] != []:
-				temp = x[1][0].get_low_resolution_url()
-  				
-  				c.append(temp)
-  		print c
-  		return render_template('index.html', listPics=c, name=myCity)
-  	return render_template('index.html', name=name)
-
-
-@app.route("/about")
-def about():
-	return getPointsCoors("New York")
-
 if __name__ == "__main__":
     app.run(debug=True)
