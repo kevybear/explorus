f9aafb38b63a4c9c9373c0d5358f1fca98e219a4
Michelle Cheung <micheung@airbears2-10-142-174-98.airbears2.1918.berkeley.edu>
2016-11-12 23:35:10 -0800

    tabs not spaces

--- instagram.py
+++ instagram.py
@@ -18,8 +18,8 @@ class InstagramLib():
 		recent_media, next_ = api.location_recent_media(location_id = loc[0].id)
 		return recent_media
 
-   	def get_photos_user(self, username):
-   		user_id = api.user_search(username)[0].id
+	def get_photos_user(self, username):
+		user_id = api.user_search(username)[0].id
 		recent_media, next_ = api.user_recent_media(user_id=user_id, count=10)
 		return recent_media
 
